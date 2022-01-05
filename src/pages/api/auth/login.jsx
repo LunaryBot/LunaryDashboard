de65f03515689b24c291259e4a064660ea10c496
JVOPINHO <jvopinho.dev@gmail.com>
2022-01-05 15:29:22 -0300

    Update in router `/api/auth/callback`

--- src/pages/api/auth/login.jsx
+++ src/pages/api/auth/login.jsx
@@ -1,4 +1,6 @@
 export default (req, res) => {
     const state = req.query.state;
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&prompt=none&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds${state ? `&state=${state}` : ''}`);
+    const callbackURL = process.env.API_AUTH_CALLBACK;
+
+    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&prompt=none&redirect_uri=${encodeURI(callbackURL)}&response_type=code&scope=identify%20guilds${state ? `&state=${state}` : ''}`);
 }
