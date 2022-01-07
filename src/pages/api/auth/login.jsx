9df5a0ebe976dbdca3d90228fbbf4759e0099f09
JVOPINHO <jvopinho.dev@gmail.com>
2022-01-07 16:37:06 -0300

    [Update] Changed redirect of router `/api/auth/login` of static url for `generateOAuth2`

--- src/pages/api/auth/login.jsx
+++
@@ -1,6 +0,0 @@
-export default (req, res) => {
-    const state = req.query.state;
-    const callbackURL = process.env.API_AUTH_CALLBACK;
-
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&prompt=none&redirect_uri=${encodeURI(callbackURL)}&response_type=code&scope=identify%20guilds${state ? `&state=${state}` : ''}`);
-}
