a6041a72f7b62fd13dc641f557332701c5f840de
JVOPINHO <jvopinho.dev@gmail.com>
2022-01-20 19:17:17 -0300

    New Util: `decode`

---
+++ src/Utils/decode.ts
@@ -0,0 +1,14 @@
+export const decode = (s: string, obj: object = {}) => {
+    obj = obj || {};
+
+    let p;
+    
+    let str = s.replace(/\+/g, ' ').replace(/;/g, '&').split('&');
+
+    for (let i = 0; i < str.length; i++) {
+        p = s[i].split('=', 2);
+        obj[unescape(p[0])] = unescape(p[1]);
+    }
+
+    return obj;
+}
