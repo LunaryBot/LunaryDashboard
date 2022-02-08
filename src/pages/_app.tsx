63e4052f9561836da990621deb2d0da0104d7112
João Pinho <joaoamoreterno1511@gmail.com>
2022-02-08 02:19:33 +0000

    Update themes system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,70 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 import Head from 'next/head';
 
-const tonalits = Object.entries({
-    "5": '0d', 
-    "10": '1a', 
-    "15": '26', 
-    "20": '33', 
-    "40": '66', 
-    "60": '99', 
-    "80": 'cc', 
-    "100": ''
-})
-
-const baseColor = '#ffffff';
-const baseColor2 = '#000000';
-const baseBandColor = '#A020F0'
-
-const GlobalStyle = createGlobalStyle`
-    :root {
-        // #f6f6f6 #0d0510
-        --background: #0d0510;
-        ${tonalits.map(([key, value]) => `--luny-colors-text-${key}: ${baseColor}${value};`).join('\n')}
-        ${tonalits.map(([key, value]) => `--luny-colors-flow-${key}: ${baseColor2}${value};`).join('\n')}
-        ${tonalits.map(([key, value]) => `--luny-colors-ui-${key}: ${baseColor}${value};`).join('\n')}
-        ${tonalits.map(([key, value]) => `--luny-colors-band-${key}: ${baseBandColor}${value};`).join('\n')}
-        ${tonalits.map(([key, value]) => `--luny-colors-light-${key}: ${baseColor}${value};`).join('\n')}
-        ${tonalits.map(([key, value]) => `--luny-colors-dark-${key}: ${baseColor2}${value};`).join('\n')}
-        --luny-colors-overlay: ${baseColor2}E6;
-    }
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    body {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        background-color: var(--background);
-        background-image: linear-gradient(180deg, var(--luny-colors-band-15) 5%, var(--background) 20%);
-        background-size: 100% 100%;
-        height: 100vh;
-        font-family: cursive;
-        color: var(--font-primary-color);
-    }
-
-    body::-webkit-scrollbar {
-        width: 5px;
-    }
-        
-    body::-webkit-scrollbar-track {
-        background: #16131B;
-        box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    }
-
-    body::-webkit-scrollbar-thumb {
-        background-color: #A020F0;
-        /*outline: 1px solid slategrey;*/
-        border-radius: 0.5em;
-    }
-    a {
-        text-decoration: none;
-    }
-`
+import '../styles/styles.css';
+import Theme from '../utils/theme';
 
 export default function MyApp({ Component, pageProps }) {
+    const GlobalStyles = createGlobalStyle`
+        :root {${Theme({ mode: pageProps.mode || 'dark' }).toString()}}
+    `
+
     return (
         <> 
             <Head>
@@ -72,7 +16,7 @@ export default function MyApp({ Component, pageProps }) {
                 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" />
                 <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet" />
             </Head>
-            <GlobalStyle />
+            <GlobalStyles id="globalstyles" />
             <Component {...pageProps} />
         </>
     )
