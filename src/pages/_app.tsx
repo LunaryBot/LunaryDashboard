8bf3d783a0235760a1a7ac3d8ae0936757643b28
JVOPINHO <jvopinho.dev@gmail.com>
2022-04-26 16:52:12 -0300

    Creating base styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/style.css'
+import { createGlobalStyle } from 'styled-components';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
 
-// This default export is required in a new `pages/_app.js` file.
+import '../styles/styles.css';
+import Theme from '../utils/theme';
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [mode, setMode] = useState<'dark'|'light'|null>(null);
+
+    useEffect(() => {
+        if(!mode) {
+            setMode(localStorage.getItem('mode') as 'dark' | 'light' | null || 'dark')
+        }
+    }, []);
+
+    const GlobalStyles = createGlobalStyle`
+        :root {${Theme({ mode: mode || 'dark' }).toString()}}
+    `
+
+    return (
+        <> 
+            <Head>
+                <title>Luna</title>
+                <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" />
+                <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet" />
+            </Head>
+            <GlobalStyles id="globalstyles" />
+            <div className={"background-gradient"} />
+            <Component {...pageProps} />
+        </>
+    )
 }
---
+++ src/utils/theme.ts
@@ -0,0 +1,80 @@
+interface IThemeColors {
+    band?: string;
+    mode?: 'dark' | 'light';
+}
+
+interface IBaseColors {
+    background: string;
+    ui: string;
+    flow: string;
+    text: string;
+    overlay: string;
+    icon: string | null;
+} 
+
+const blackColor = '#000000';
+const whiteColor = '#ffffff';
+
+const basesColors = {
+    dark: {
+        background: '#0d0510',
+        ui: whiteColor,
+        flow: blackColor,
+        text: whiteColor,
+        overlay: blackColor,
+        icon: whiteColor
+    },
+    light: {
+        background: '#faecff',
+        ui: blackColor,
+        flow: whiteColor,
+        text: blackColor,
+        overlay: whiteColor,
+        icon: null
+    },
+};
+
+const tonalits = Object.entries({
+    "5": '0d', 
+    "10": '1a', 
+    "15": '26', 
+    "20": '33', 
+    "40": '66', 
+    "60": '99', 
+    "80": 'cc', 
+    "100": ''
+});
+
+function ThemeVariables({ band = "#A020F0", mode = 'dark' }: IThemeColors = {}) {
+    const baseColors: IBaseColors = basesColors[mode] || basesColors['dark'];
+    
+    const obj = {
+        "--luny-colors-background": baseColors.background,
+        ...mapTonalits("--luny-colors-band", band),
+        ...mapTonalits("--luny-colors-ui", baseColors.ui),
+        ...mapTonalits("--luny-colors-flow", baseColors.flow),
+        ...mapTonalits("--luny-colors-text", baseColors.text),
+        "--luny-colors-overlay": baseColors.overlay + "E6",
+        "--luny-colors-icon": baseColors.icon || band,
+        ...mapTonalits("--luny-colors-light", whiteColor),
+        ...mapTonalits("--luny-colors-dark", blackColor),
+        "--luny-colors-green": "#61fe80",
+        "--luny-colors-red": "#fe4854"
+    };
+
+    return { 
+        ...obj,
+        toString: () => {
+            return Object.entries({ ...obj }).map(([key, value]) => `${key}:${value};`).join("")
+        }
+    };
+};
+
+function mapTonalits(key: string, color: string) {
+    return  Object.fromEntries(
+        tonalits.map(([tonalit, value]) => [`${key}-${tonalit}`, `${color}${value}`])
+    );
+};
+    
+export default ThemeVariables;
+module.exports.mapTonalits = mapTonalits;
