6b0654d3a55e50dbcdaeea285cf651346d4bba0f
victorpantarotti <victorpantarotti@gmail.com>
2022-04-29 17:32:22 -0300

    NavBar finished

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,17 +5,39 @@ export default class NavBar extends Component {
     render() {
         return (
             <>
-                <div className={Styles.container}>
+                <div className={Styles.navBarContainer}>
                     <div className={Styles.box}>
                         <div className={`${Styles.content} ${Styles.firstContentBox}`}>
                             <img 
                                 src='https://images-ext-1.discordapp.net/external/9v1spCjx1AnB9D2-OiSEvSzrXpORzx8gScClAcMhX_A/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/807225861769396225/60bab4386da65e5332379ddd2461e834.png' 
-                                width={"44px"}
-                                style={{
-                                    marginTop: '5px',
-                                    borderRadius: '200px'
-                                }}
+                                className={Styles.lunarImg}
                             />
+                            <div className={Styles.quickLinks}>
+                                <a
+                                    className={Styles.lunarTxt}
+                                    href='#'
+                                    >
+                                    Lunar
+                                </a>
+                                <a 
+                                    className={Styles.link}
+                                    href='#'
+                                >
+                                    Comandos
+                                </a>
+                                <a 
+                                    className={Styles.link}
+                                    href='#'
+                                >
+                                    Invite
+                                </a>
+                                <a 
+                                    className={Styles.link}
+                                    href='#'
+                                >
+                                    Vote
+                                </a>
+                            </div>
                         </div>
                         <div className={`${Styles.content} ${Styles.secondContentBox}`}>
                             <button
