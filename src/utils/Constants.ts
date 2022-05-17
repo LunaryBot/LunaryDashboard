a2ffac0b9a6d3bbbd0371dce25a033f5467f4c5b
JVOPINHO <jvopinho.dev@gmail.com>
2022-05-17 10:35:22 -0300

    Added `Constants.ChannelTypes`

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -12,5 +12,35 @@ export const URLS = {
                 icon: 'far fa-grip-vertical'
             }
         ]
+    },
+    GUILD: {
+        general: [
+            {
+                label: 'Home',
+                url: '/dashboard/guilds/[guild]/',
+                icon: 'far fa-home'
+            },
+            {
+                label: 'Moderação',
+                url: '/dashboard/guilds/[guild]/moderation',
+                icon: 'far fa-hammer'
+            }
+        ]
     }
+}
+
+export const ChannelTypes = {
+    GUILD_TEXT: 0,
+    DM: 1,
+    GUILD_VOICE: 2,
+    GROUP_DM: 3,
+    GUILD_CATEGORY: 4,
+    GUILD_NEWS: 5,
+    GUILD_STORE: 6,
+    GUILD_NEWS_THREAD: 10,
+    GUILD_PUBLIC_THREAD: 11,
+    GUILD_PRIVATE_THREAD: 12,
+    GUILD_STAGE_VOICE: 13,
+    GUILD_DIRECTORY: 14,
+    GUILD_FORUM: 15,
 }
