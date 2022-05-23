c67d2cc26f475ae908170f5e1000303e1cdf2e5f
JVOPINHO <jvopinho.dev@gmail.com>
2022-05-23 17:25:18 -0300

    Updating SideBar Component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,27 +4,37 @@ import { URLS } from '../utils/Constants';
 import Link from 'next/link';
 import { IGuildData, IUser } from '../@types';
 import Utils from '../utils/Utils';
+import APIUtils from '../utils/APIUtils';
+import Dots from '../components/Dots';
 
 interface IState {
     sidebarOpened: boolean;
+    sidebarServersMenuOpened: boolean;
     urls: {
         [category: string]: { url: string; label: string, icon: string }[];
     };
     user: IUser;
     guild: IGuildData | null;
+    guilds: IGuildData[];
 }
 
 class SideBar extends React.Component {
     public state: IState;
-    constructor(props: { urlsType: 'USER', user: IUser, guild?: IGuildData }) {
+    private api: APIUtils;
+
+    constructor(props: { urlsType: 'USER' | 'GUILD', user: IUser, guild?: IGuildData, api: APIUtils }) {
         super(props);
 
         this.state = {
             sidebarOpened: true,
-            urls: URLS[props.urlsType],
+            sidebarServersMenuOpened: false,
+            urls: URLS[props.urlsType || 'USER'],
             user: props.user,
-            guild: props.guild || null
-        };
+            guild: props.guild || null,
+            guilds: null,
+        }
+
+        this.api = props.api;
     }
     componentDidMount() {
         const body = document.querySelector('body'),
@@ -32,7 +42,9 @@ class SideBar extends React.Component {
             toggle = body.querySelector('.toggle'),
             searchBtn = body.querySelector('.searchBox'),
             modeSwitch = body.querySelector('.sunMoon'),
-            modeIcon = body.querySelector('.modeIcon') as HTMLElement;
+            modeIcon = body.querySelector('.modeIcon') as HTMLElement,
+            userInfo = body.querySelector('.userInfo') as HTMLElement,
+            serversMenu = body.querySelector('.serversMenu') as HTMLElement;
             
             localStorage.getItem('sidebarOpened') === 'true' ? sidebar.classList.remove('close') : sidebar.classList.add('close');
 
@@ -61,6 +73,33 @@ class SideBar extends React.Component {
             modeIcon.classList[mode == 'light' ? 'add' : 'remove']('fa-moon');
             modeIcon.classList[mode == 'dark' ? 'add' : 'remove']('fa-sun');
         });
+
+        document.addEventListener('click', (e) => {
+            if(this.state.sidebarServersMenuOpened && !serversMenu.contains(e.target as Node) && !userInfo.contains(e.target as Node)) {
+                this.toggleServersMenu();
+            }
+        });
+    }
+
+    async componentDidUpdate(_, prevState: IState) {
+        if(prevState.sidebarServersMenuOpened !== this.state.sidebarServersMenuOpened && this.state.sidebarServersMenuOpened && !this.state.guilds) {
+            const guilds = await this.api.guilds();
+
+            console.log(guilds);
+
+            this.setState({ guilds });
+
+            const serversMenu = document.querySelector('.serversMenuContent') as HTMLElement;
+            const selected = serversMenu.querySelector(`[data-guild="${this.guild?.id || this.user?.id}"]`) as HTMLElement;
+
+            if(selected) {
+                serversMenu.scrollTop = selected.offsetTop - serversMenu.offsetTop;
+            }
+        }
+    }
+    
+    toggleServersMenu() {
+        this.setState({ sidebarServersMenuOpened: !this.state.sidebarServersMenuOpened });
     }
 
     get user() {
@@ -77,44 +116,78 @@ class SideBar extends React.Component {
                 <nav className={'sidebar close'}>
                     <header>
                         <div className={'container'}>
-                            <span className={'image'}>
-                                {this.guild ? ( this.guild?.icon ? <img src={Utils.getGuildIcon(this.guild, { size: 1024, dynamic: true })} /> : (<div>{Utils.stringAcronym(this.guild.name)}</div>)) : ( <img src={Utils.getUserAvatar(this.user, { size: 1024, dynamic: true })} /> )}
-                            </span>
+                            {this.state.user || this.state.guild ? (
+                                <div className={'userInfo'} onClick={() => this.toggleServersMenu()} data-opened={this.state.sidebarServersMenuOpened}>
+                                    <span className={'image'}>
+                                        {this.guild ? ( this.guild?.icon ? <img src={Utils.getGuildIcon(this.guild, { size: 1024, dynamic: true })} /> : (<div>{Utils.stringAcronym(this.guild.name)}</div>)) : ( <img src={Utils.getUserAvatar(this.user, { size: 1024, dynamic: true })} /> )}
+                                    </span>
 
-                            <div className={'text'}>
-                                <span className={'name'}>{this.guild?.name || this.user?.username}</span>
-                                <span className={'id'}>{this.guild?.id || this.user?.id}</span>
-                            </div>
+                                    <div className={'text'} style={{marginLeft: '10px'}}>
+                                        <span className={'name'}>{this.guild?.name || this.user?.username}</span>
+                                        <span className={'id'}>{this.guild?.id || this.user?.id}</span>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className={'userInfo'}>
+                                    <Dots />
+                                </div>
+                            )}
 
-                            <section className={'menu'}>
+                            <section className={'serversMenu'} data-opened={this.state.sidebarServersMenuOpened}>
+                                <div className={'serversMenuContent'}>
                                     <ul>
-                                        <li>
-                                            <div className={'container svMenu'}>
-                                                <a className={'btn'} href={`#`}>
-                                                    <span className={'image'}>
-                                                        {this.guild ? ( this.guild?.icon ? <img src={Utils.getGuildIcon(this.guild, { size: 1024, dynamic: true })} /> : (<div>{Utils.stringAcronym(this.guild.name)}</div>)) : ( <img src={Utils.getUserAvatar(this.user, { size: 1024, dynamic: true })} /> )}
-                                                    </span>
-                                                    <div className={'text'}>
-                                                        <span className={'name'}>{this.guild?.name || this.user?.username}</span>
+                                        <li data-guild={this.user?.id} data-selected={!this.guild}>
+                                            <Link href={'/dashboard/@me'}>
+                                                <a>
+                                                    <div className={'server'}>
+                                                        <span className={'image'}>
+                                                            <img src={Utils.getUserAvatar(this.user, { size: 1024, dynamic: true })} />
+                                                        </span>
+
+                                                        <span className={'text'}>
+                                                            <span className={'name'}>{this.user?.username}</span>
+                                                        </span>
                                                     </div>
-                                                </a>  
-                                            </div>
+                                                </a>
+                                            </Link>  
                                         </li>
+
                                         <hr />
-                                        <li>
-                                            <div className={'container svMenu'}>
-                                                <a className={'btn'} href={`#`}>
-                                                    <span className={'image'}>
-                                                        {this.guild ? ( this.guild?.icon ? <img src={Utils.getGuildIcon(this.guild, { size: 1024, dynamic: true })} /> : (<div>{Utils.stringAcronym(this.guild.name)}</div>)) : ( <img src={Utils.getUserAvatar(this.user, { size: 1024, dynamic: true })} /> )}
-                                                    </span>
-                                                    <div className={'text'}>
-                                                        <span className={'name'}>{this.guild?.name || this.user?.username}</span>
+                                        {this.state.guilds?.filter(guild => guild.access)?.map(guild => (
+                                            <li key={guild.id} data-guild={guild.id} data-selected={[this.guild?.id || this.user?.id].includes(guild.id)}>
+                                                    <Link href={`/dashboard/guilds/${guild.id}`}>
+                                                        <a>
+                                                            <div className={'server'}>
+                                                                <span className={'image'}>
+                                                                    {guild?.icon ? <img src={Utils.getGuildIcon(guild, { size: 1024, dynamic: true })} /> : (<div>{Utils.stringAcronym(guild.name)}</div>)}
+                                                                </span>
+
+                                                                <span className={'text'}>
+                                                                    <span className={'name'}>{guild?.name}</span>
+                                                                </span>
+                                                            </div>
+                                                        </a>
+                                                    </Link>  
+                                            </li>
+                                        )) ?? (<Dots/>)}
+                                        <hr />
+
+                                        <li className={'invite'}>
+                                                <a>
+                                                    <div className={'server'}>
+                                                        <span className={'image'}>
+                                                            <img src={'https://imgur.com/xCIgS2n.png'} style={{borderRadius: '0'}} />
+                                                        </span>
+
+                                                        <span className={'text'}>
+                                                            <span className={'name'}>Invite</span>
+                                                        </span>
                                                     </div>
-                                                </a>  
-                                            </div>
+                                                </a>
                                         </li>
                                     </ul>
-                                </section>
+                                </div>
+                            </section>
                         </div>
 
                         <i className={'bx bx-chevron-right toggle'} />
--- src/utils/APIUtils.ts
+++ src/utils/APIUtils.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
-import { IUser } from '../@types';
+import { IGuild, IGuildData, IMember, IUser } from '../@types';
 
 class APIUtils {
     public authorization: string;
     public axios: AxiosInstance;
+    public guildId: string;
 
-    constructor(authorization: string, baseURL: string) {
+    constructor(authorization: string, baseURL: string, guildId?: string) {
         this.authorization = authorization;
 
         this.axios = axios.create({
@@ -15,11 +16,23 @@ class APIUtils {
             }
         });
 
-        console.log(baseURL);
+        this.guildId = guildId;
     }
 
     public async user(): Promise<IUser> {
         const response = await this.axios.get('/users/@me');
+
+        return response.data;
+    }
+
+    public async connect(): Promise<{ user: IUser, guild?: IGuild, member?: IMember }> {
+        const response = await this.axios.get(this.guildId ? `/guilds/${this.guildId}` : '/users/@me');
+        
+        return response.data;
+    }
+
+    public async guilds(): Promise<IGuildData[]> {
+        const response = await this.axios.get('/users/@me/guilds');
 
         return response.data;
     }
