bf0d815fe88c81324b3196cb1ddd63ecc213fe43
jvopinho <joaoamoreterno1511@gmail.com>
2022-08-29 23:26:17 +0000

    Starting Themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,30 +7,32 @@ import { DashboardLayout } from '../components/dashboard';
 
 import Theme from '../utils/theme';
 
+import themesStyle from '../styles/theme.module.scss';
+
 export default function MyApp({ Component, pageProps }) {
     const [_mode, setMode] = useState<'dark'|'light'|null>(null);
 
     useEffect(() => {
-        if(!_mode) {
-            setMode(localStorage.getItem('mode') as 'dark' | 'light' | null || 'dark')
-        }
-
-        const mode = (localStorage.getItem('mode') as 'dark' | 'light' | null) || 'dark';
-
         const body = document.querySelector('body');
 
-        body.setAttribute('data-mode', mode);
+        body.classList.add(themesStyle.root);
+
+        console.log(themesStyle.root)
         
+        if(!_mode) {
+            setMode(localStorage.getItem('theme') as 'dark' | 'light' | null || 'dark')
+        }
+
+        const mode = (localStorage.getItem('theme') as 'dark' | 'light' | null) || 'dark';
+
+        body.setAttribute('data-theme', mode);
+
         window.changeMode = (mode) => {
             localStorage.setItem('mode', mode);
-            document.querySelector('[data-styled]').innerHTML = `:root {${Theme({ mode: mode || 'dark' }).toString()}}`
-            body.setAttribute('data-mode', mode);
+            // document.querySelector('[data-styled]').innerHTML = `:root {${Theme({ mode: mode || 'dark' }).toString()}}`
+            body.setAttribute('data-theme', mode);
         }
     }, []);
-
-    const GlobalStyles = createGlobalStyle`
-        :root {${Theme({ mode: _mode || 'dark' }).toString()}}
-    `
 
     return (
         <> 
@@ -40,7 +42,6 @@ export default function MyApp({ Component, pageProps }) {
                 <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet" />
                 <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet' />
             </Head>
-            <GlobalStyles id="globalstyles" />
             <div className={"backgroundGradient"} />
             <DashboardLayout>
                 <Component {...pageProps} />   
