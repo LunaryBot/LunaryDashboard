a0b253a60871dc7252fe8d2397859759e9f0f66c
jvopinho <jvopinho.dev@gmail.com>
2022-10-13 19:18:57 -0300

    Adding Dots component

---
+++ src/components/Dots.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const Dots = styled.div`
+    width: 100%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > div {
+        width: 20px;
+        height: 20px;
+        background: #8385aa;
+        border-radius: 50%;
+        margin: 3px;
+        animation: loader 0.7s infinite alternate;
+    }
+
+    & > div:nth-child(2) {
+    animation-delay: 0.2s;
+}
+
+    & > div:nth-child(3) {
+        animation-delay: 0.4s;
+    }
+
+    @keyframes loader {
+        to {
+            opacity: 0.1;
+            background: var(--luny-band-100);
+        }
+    }
+`;
+
+export default () => (
+    <Dots>
+        <div />
+        <div />
+        <div />
+    </Dots>
+)
