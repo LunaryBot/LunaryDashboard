8de456b6b35a80a5c0c271f9216425d9894c3197
jvopinho <jvopinho.dev@gmail.com>
2022-11-02 11:36:17 -0300

    Adding AbstractGuild type

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,8 +1,17 @@
-export interface User {
-	username: string;
+export interface AbstractGuild {
 	id: string;
+	name: string;
+	icon: string | null;
+	features: string[];
+	owner: boolean;
+	permissions: number;
+}
+
+export interface User {
+	id: string;
+	username: string;
 	discriminator: string;
-	avatar: string|null;
+	avatar: string | null;
 	public_flags: number;
 	banner?: string;
 	banner_color?: string;
@@ -12,4 +21,8 @@ export interface User {
 	email?: string;
 	mfa_enabled?: boolean;
 	verified?: boolean;
+}
+
+export interface UserWithGuilds extends User {
+	guilds: AbstractGuild[] | null;
 }
