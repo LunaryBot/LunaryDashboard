8a6c32522532fa543cff3446747a051d6c3aa6b1
jvopinho <jvopinho.dev@gmail.com>
2022-11-02 10:02:38 -0300

    Starting types refactoring

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,4 +1,4 @@
-export interface IUser {
+export interface User {
 	username: string;
 	id: string;
 	discriminator: string;
@@ -12,72 +12,4 @@ export interface IUser {
 	email?: string;
 	mfa_enabled?: boolean;
 	verified?: boolean;
-}
-
-export interface IGuildData {
-	icon: string;
-	id: string;
-	name: string;
-	owner: boolean;
-	permissions: number;
-	features: string[];
-	permissions_new: string;
-	access?: boolean;
-	favorite?: boolean;
-}
-
-export interface IChannel {
-	id: string;
-	name: string;
-	type: number;
-	deleted: boolean;
-	guildID: string;
-	guild: string;
-	parentID: string;
-	permissionOverwrites: string[];
-	nsfw: boolean;
-	position: number;
-	rawPosition: number;
-	topic: string|null;
-	lastMessageID: string|null;
-	rateLimitPerUser: number;
-	createdTimestamp: number;
-}
-
-export interface IRole {
-	id: string;
-	name: string;
-	color: number;
-	hoist: boolean;
-	position: number;
-	permissions: number;
-	managed: boolean;
-	mentionable: boolean;
-	createdTimestamp: number;
-	deleted: boolean;
-	icon: string|null;
-	rawPosition: number;
-	unicodeEmoji: boolean;
-	guild: string;
-}
-
-export interface IGuild {
-	name: string;
-	id: string;
-	icon: string;
-	channels: IChannel[];
-	roles: IRole[];
-	shardID: number;
-	cluserID: string;
-    features: string[];
-}
-
-export interface IMember {
-	id: string;
-	createdAt: number;
-	communicationDisabledUntil: number;
-	joinedAt: number;
-	nick: string|null;
-	roles: string[];
-	permissions: number;
 }
