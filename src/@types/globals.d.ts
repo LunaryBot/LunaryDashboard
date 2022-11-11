bff196d684d4b7c5b1de8d203d1de34a4fe294af
jvopinho <jvopinho.dev@gmail.com>
2022-11-11 00:56:14 -0300

    Fixing env variables

--- src/@types/globals.d.ts
+++ src/@types/globals.d.ts
@@ -4,7 +4,7 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             DISCORD_CLIENT_ID: string;
-            OAUTH_REDIRECT_URI: string;
+            OAUTH2_DISCORD_REDIRECT_URI: string;
             API_URL: string;
             NODE_ENV: 'development' | 'production';
         }
