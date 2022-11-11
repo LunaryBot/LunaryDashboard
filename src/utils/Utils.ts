d7a0701dede6a238afcbce06c4f55450a8cfd773
jvopinho <jvopinho.dev@gmail.com>
2022-11-11 00:56:32 -0300

    Adding Utils.generateOAuth2Discord

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,4 +1,30 @@
-import { AbstractGuild } from "../@types";
+import { AbstractGuild } from '../@types';
+
+type TScope = 
+	'applications.builds.read' 
+	| 'applications.commands'
+	| 'applications.entitlements' 
+	| 'applications.store.update' 
+	| 'bot' 
+	| 'connections' 
+	| 'email' 
+	| 'identify' 
+	| 'guilds' 
+	| 'guilds.join' 
+	| 'gdm.join' 
+	| 'webhook.incoming'
+
+interface DiscordOAuth2 {
+    clientId: string;
+    scopes: TScope[];
+    permissions?: bigint | number;
+    guildId?: string;
+    redirectUri?: string;
+    state?: string;
+    responseType?: string;
+    prompt?: string;
+    disableGuildSelect?: boolean;
+}
 
 export class Utils {
     static stringAcronym(string: string): string {
@@ -14,5 +40,46 @@ export class Utils {
         } else {
             return undefined;
         }
+    }
+
+    static generateOAuth2Discord({
+        clientId = process.env.DISCORD_CLIENT_ID,
+        scopes,
+        permissions = BigInt(0),
+        guildId = null,
+        redirectUri = '/',
+        responseType = 'code',
+        state = null,
+        disableGuildSelect = false,  
+        prompt = null
+    }: DiscordOAuth2) {
+        const query = new URLSearchParams({
+            client_id: clientId,
+            scope: scopes.join(' '),
+        });
+    
+        if (permissions) {
+            query.set('permissions', Number(permissions).toString());
+        };
+    
+        if (guildId) {
+            query.set('guild_id', guildId);
+            if(disableGuildSelect) {
+                query.set('disable_guild_select', 'true');
+            }
+        };
+    
+        if(redirectUri) {
+            query.set('redirect_uri', redirectUri);
+            query.set('response_type', responseType);
+            if(state) {
+                query.set('state', state);
+            };
+            if(prompt) {
+                query.set('prompt', prompt);
+            };
+        };
+    
+        return `https://discord.com/api/oauth2/authorize?${query.toString()}`;
     }
 }
