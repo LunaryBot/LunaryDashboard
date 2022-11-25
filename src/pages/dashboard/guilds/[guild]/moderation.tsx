5828cbdc3b7de13894a9ff5a7917d855df80db4f
jvopinho <jvopinho.dev@gmail.com>
2022-11-25 00:10:37 -0300

    Updating base from `dashboard/guilds/[guild]/moderation`

--- src/pages/dashboard/guilds/[guild]/moderation.tsx
+++ src/pages/dashboard/guilds/[guild]/moderation.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { PropsWithChildren, useState } from 'react';
+import styled from 'styled-components';
+
 import { useAPI } from '../../../../hooks/useAPI';
 
 import { Switch, Card, Select } from '../../../../components';
 
-const DashboardGuild: React.FC = () => {
+import { PermissionFlagsBits } from 'discord-api-types/v10';
+
+import styles from '../../../../styles/Guild.module.scss';
+
+const flagsEntries = Object.entries(PermissionFlagsBits);
+
+const ToggleBox: React.FC<PropsWithChildren & { defaultValue?: boolean }> = (props) => {
+    const [value, setValue] = useState<boolean>(!!props?.defaultValue);
+
+    return (
+        <div className={styles.toggleBox} {...(value ? {'data-checked': true} : {})} onClick={() => setValue(!value)}>
+            <div className={styles.checkedIcon}><i className={'fas fa-check'}/></div>
+            {props.children}
+        </div>
+    )
+}
+
+const DashboardGuildModeration: React.FC = () => {
     const { user, guild } = useAPI();
 
     console.log();
@@ -25,8 +44,66 @@ const DashboardGuild: React.FC = () => {
                     />
                 </Card.Content>
             </Card>
+
+            <Card size={'small'}>
+                <Card.Header>
+                    <h3>Canal de Modlogs</h3>
+                </Card.Header>
+                <Card.Content>
+                    <Select
+                        customId='modlogs_channel'
+                        options={[...(guild?.channels || [])].sort((a, b) => a.position - b.position).map(channel => ({
+                            label: channel.name,
+                            value: channel.id,
+                        })) || []}
+                        placeholder={'Select a channel'}
+                    />
+                </Card.Content>
+            </Card>
+
+            <Card retractable={true}>
+                <Card.Header>
+                    <h2>
+                        <code style={{backgroundColor: 'transparent', border: 'none', fontSize: '16px'}}>
+                            <i className={'fas fa-slash'} style={{transform: 'rotate(90deg)', marginRight: '0', color: 'var(--luny-band-100)'}} />
+                            ban user {/* */}
+                            <code style={{marginRight: '5px'}}>user</code>
+                            <code style={{marginRight: '5px'}}>reason</code>
+                            <code style={{marginRight: '5px'}}>notify-dm</code>
+                            <code style={{marginRight: '5px'}}>days</code>
+                        </code>
+                        <br />
+                        <span>Ban a user from the guild.</span>
+                    </h2>
+                </Card.Header>
+                <Card.Content>
+                    <Select 
+                        options={flagsEntries.map(([key, value]) => {
+                            return ({
+                                label: key.replace(/([A-Z])|^([a-z])/g, (w) => ` ${w.toUpperCase()}`),
+                                value: value,
+                                default: key == ''
+                            })
+                        })}
+                        customId={'ban_command_permissions'}
+                        placeholder={'Select a permission'}
+                        maxValues={flagsEntries.length}
+                    />
+
+                    <div className={styles.togglesBoxWrapper}>
+                        <ToggleBox>
+                            <h3>Manatory Reason</h3>
+                            <span>Requires a reason for the ban.</span>
+                        </ToggleBox>
+                        <ToggleBox>
+                            <h3>Notify User</h3>
+                            <span>Notify the user that they have been banned.</span>
+                        </ToggleBox>
+                    </div>
+                </Card.Content>
+            </Card>
         </main>
     )
 }
 
-export default DashboardGuild;
+export default DashboardGuildModeration;
