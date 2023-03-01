bc17cb88737c889214b9d0abfd99190e1f61e382
jvopinho <jvopinho.dev@gmail.com>
2023-03-01 00:03:09 -0300

    Adding Utils.uuid

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -27,6 +27,23 @@ interface DiscordOAuth2 {
 }
 
 export class Utils {
+    static uuid(): string {
+        let d = Date.now();
+        let d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;
+
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            let r = Math.random() * 16;
+            if(d > 0) {
+                r = (d + r) % 16 | 0;
+                d = Math.floor(d / 16);
+            } else {
+                r = (d2 + r) % 16 | 0;
+                d2 = Math.floor(d2 / 16);
+            }
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
+    
     static stringAcronym(string: string): string {
         return string
             .replace(/'s /g, ' ')
