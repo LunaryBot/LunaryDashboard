c0efbe11e9fa793690b1caaa6d41126295c70cf7
jvopinho <jvopinho.dev@gmail.com>
2023-03-23 23:41:00 -0300

    Adding TestAPIContext#fetchGuild

--- src/contexts/TestAPIContext.tsx
+++ src/contexts/TestAPIContext.tsx
@@ -16,8 +16,8 @@ interface APIContextData {
     guild?: Guild;
 
     checkGuild: (guildId: string) => boolean;
-    // fetchGuild: (id: string) => Promise<Guild[]>;
-    // fetchUserGuilds: () => Promise<AbstractGuild[]>;
+    fetchGuild: (id: string) => Promise<Guild[]>;
+    fetchUserGuilds: () => Promise<AbstractGuild[]>;
 }
 
 const APIContext = createContext<APIContextData>({} as APIContextData);
@@ -40,14 +40,76 @@ export class APIProvider extends React.Component<React.PropsWithChildren, {
         }
     }
 
+    async componentDidUpdate() {
+        const pathname = window.location.pathname;
+
+        if(guildUrlString.test(pathname)) {
+            const guildId = pathname.replace(guildUrlString, '$1');
+
+            if(this.state.guild?.id !== guildId) {
+                await this.fetchGuild(guildId);
+            }
+        } else if(this.state.guild) {
+            this.setState({ guild: null });
+        }
+    }
+
     checkGuild(guildId: string) {
         return this.state.guild?.id === guildId;
+    }
+
+    async fetchGuild(id: string) {
+        const guild: Guild = {
+            id: 'id',
+            name: '🌇 Lunar City',
+            banner: null,
+            channels: [
+                {
+                    id: '002',
+                    name: 'rules',
+                    nsfw: false,
+                    parent_id: '001',
+                    position: 1,
+                    type: 0,
+                },
+                {
+                    id: '005',
+                    name: 'news',
+                    nsfw: false,
+                    parent_id: '001',
+                    position: 1,
+                    type: 5,
+                },
+                {
+                    id: '004',
+                    name: 'chat',
+                    nsfw: false,
+                    parent_id: '003',
+                    position: 1,
+                    type: 0,
+                },
+            ],
+            roles: [],
+            features: ['ANIMATED_ICON'],
+            icon: 'a_e32c439c0f444ee342ff89e631957af9',
+            owner_id: '842170079520096276'
+        };
+
+        this.setState({ guild });
+
+        return guild;
+    }
+
+    async fetchUserGuilds() {
+        
     }
 
     render(): React.ReactNode {
         const { 
             props: { children },
             state: { loading, user, guild },
+            fetchUserGuilds,
+            fetchGuild,
             checkGuild,
         } = this;
 
@@ -58,6 +120,8 @@ export class APIProvider extends React.Component<React.PropsWithChildren, {
                 user, 
                 guild,
                 checkGuild: checkGuild.bind(this),
+                fetchUserGuilds: fetchUserGuilds.bind(this), 
+                fetchGuild: fetchGuild.bind(this),
             }}>
                 {children}
             </APIContext.Provider>
